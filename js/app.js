e54dfc28e81f55afefb9162b92cd25fd652f07c8
s-imad <87981412+s-imad@users.noreply.github.com>
2022-06-30 13:06:50 +0200

    finition page parfum

--- js/app.js
+++ js/app.js
@@ -7,3 +7,18 @@ function toggleMenu() {
     });
 }
 toggleMenu();
+
+// fonction qui permet de faire apparaitre les parfums en fonction de la barre de recherche
+function rechercheParfum() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('parfums');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
